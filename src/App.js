0238e11d1460912d64bac38d906cbc34370bfce5
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-04 10:04:53 -0800

    added state components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [allMovies, setAllMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [movieFormYearReleased, setMovieFormYearREleased] = useState('');
+  const [movieFormDirector, setMovieFormDirector] = useState('');
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieFormColor, setMovieFormColor] = useState('pink');
+
   return (
     <div className="App">
       
